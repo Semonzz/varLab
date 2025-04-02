d84fcc95599b4f7900c4c8275fcf3a0640879ddc
Semonzz <evgenseme045@gmail.com>
2025-04-03 00:11:29 +0300

    Finish tasks

--- script.sh
+++ script.sh
@@ -7,6 +7,7 @@ main(){
         echo "3. Print file"
         echo "4. Edit file with echo"
         echo "5. Edit file with nano"
+        echo "6. Directoris and files"
         echo "7. Quit"
         echo
         read -p "Choose one: " choice
@@ -17,8 +18,9 @@ main(){
             "3" ) prntFile;;
             "4" ) editEcho;;
             "5" ) editNano;;
+            "6" ) dirAndFiles;;
             "7" ) exit 0;;
-            * ) echo "Smth wrong!";;
+            * ) echo "No choice!!";;
         esac
     done
 }
@@ -92,6 +94,142 @@ editNano() {
     nano "$file"
     echo "Succesful"
     echo
+}
+
+dirAndFiles() {
+    echo
+    echo "1. Create directory"
+    echo "2. Remove directory"
+    echo "3. Rename file or directory"
+    echo "4. Create file"
+    echo "5. Remove file"
+    echo "6. Copy file or directory"
+    echo "7. Move file or directory"
+    echo 
+    read -p "Choose one: " choice
+    echo 
+    case $choice in
+        "1" ) crtDir;;
+        "2" ) rmvDir;;
+        "3" ) rnmDirAndFile;;
+        "4" ) crtFile;;
+        "5" ) rmvFile;;
+        "6" ) cpyDirAndFile;;
+        "7" ) mvDirAndFile;;
+        * ) echo "No choice!!"
+    esac
+}
+
+crtDir() {
+    path=$(inputPath)
+    if checkPath "$path"; then
+        echo "Already exists!"
+        echo
+    else 
+        mkdir "$path"
+        echo "Succesful!"
+        echo
+    fi
+}
+
+rmvDir() {
+    path=$(inputPath)
+    if checkPath "$path"; then
+        rm -r "$path"
+        echo "Successful"
+        echo
+    else 
+        echo "No such directory!"
+        echo
+    fi
+}
+
+inputName() {
+    read -e -p "Enter a file or directory name: " name  #-e for Tab
+    echo $name
+}
+
+rnmDirAndFile() {
+    name=$(inputName)
+    read -e -p "Enter a new name: " text
+
+    if checkPath "$name" || checkFile "$name"; then
+        mv "$name" "$text"
+        echo "Successful"
+        echo
+    else 
+        echo "No such file or directory!"
+        echo
+    fi
+}
+
+crtFile() {
+    file=$(inputFile)
+    if checkFile "$file"; then
+        echo "Already exists!"
+        echo
+    else 
+        touch "$file"
+        echo "Succesful!"
+        echo
+    fi
+}
+
+rmvFile() {
+    file=$(inputFile)
+    if checkFile "$file"; then
+        rm "$file"
+        echo "Successful"
+        echo
+    else 
+        echo "No such directory!"
+        echo
+    fi
+}
+
+cpyDirAndFile() {
+    toCpy=$(inputName)
+    whrCpy=$(inputPath)
+    if checkFile "$toCpy"; then 
+        if checkPath "$whrCpy"; then 
+            cp "$toCpy" "$whrCpy"
+            echo "Success!"
+            echo
+        else 
+            echo "Destination error!"
+            echo
+        fi
+    elif checkPath "$toCpy"; then 
+        if checkPath "$whrCpy"; then 
+            cp -R "$toCpy" "$whrCpy"
+            echo "Success!"
+            echo
+        else 
+            echo "Destination error!"
+            echo
+        fi
+    else 
+        echo "No such file or directory!"
+        echo
+    fi
+}
+
+mvDirAndFile() {
+    toMv=$(inputName)
+    whrMv=$(inputPath)
+    if checkFile "$toMv" || checkPath "$toMv"; then 
+        if checkPath "$whrMv"; then 
+            mv "$toMv" "$whrMv"
+            echo "Success!"
+            echo
+        else 
+            echo "Destination error!"
+            echo
+        fi
+    else 
+        echo "No such file or directory!"
+        echo
+    fi
 }
 
 main
